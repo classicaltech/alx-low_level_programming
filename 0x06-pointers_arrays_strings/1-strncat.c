1039af8ee0d493ab832792cd27a295265e5bb8e7
adetoke <adetokebello60@gmail.com>
2022-12-23 12:01:24 -0800

    Task one

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,32 +1,30 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates n bytes of one string to another
- * @src: string to be appended
- * @dest: string to be updated
- * @n: bytes of str to be appended
- *
- * Return: pointer to dest
+ * _strcat - joins two strings with n bytes
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of chat to be copied
+ * Return: dest
  */
+
 char *_strcat(char *dest, char *src, int n)
 {
-	char *d = dest;
 	int i;
+	int j;
 
-	if (*src != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		while (*dest != '\0')
-		{
-			dest++;
-		}
-		i = 0;
-		while (i < n)
-		{
-			*dest = *src;
-			dest++;
-			src++;
-			i++;
-		}
+		i++;
 	}
-	return (d);
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
