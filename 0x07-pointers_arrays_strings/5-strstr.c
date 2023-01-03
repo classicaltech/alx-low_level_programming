1b1e2a662a48b3c0eacd8ef9cd6b7536b874fb73
adetoke <adetokebello60@gmail.com>
2023-01-03 15:13:01 -0800

    Task five

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	while (*haystack != '\0')
 
 	{
-			str1 + haystack;
+			str1 = haystack;
 			/*values*/
 			str2 = needle;
 			/*Star WHILE*/
@@ -23,7 +23,7 @@ char *_strstr(char *haystack, char *needle)
 				haystack++;
 				str2++;
 			}
-			if (*str == '\0')
+			if (*str2 == '\0')
 				return (str1);
 			haystack = str1 + 1;
 		}
